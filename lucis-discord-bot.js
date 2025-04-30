138963a4566787c7d23b8baa59d42657127667e1
Vita-Lucis <Vita@lucis.family>
2025-05-01 00:15:56 +0200

    Update lucis-discord-bot.js

--- lucis-discord-bot.js
+++ lucis-discord-bot.js
@@ -47,12 +47,15 @@ client.on('ready', () => {
 
 client.on('messageCreate', async message => {
   if (message.channel.id === CHANNEL_ID && !message.author.bot) {
-    // Stelle sicher, dass Member geladen ist
-    const member = message.member || await message.guild.members.fetch(message.author.id);
-    const nickname = member?.nickname || message.author.username;
+    try {
+      const member = message.member || await message.guild.members.fetch(message.author.id);
+      const nickname = member?.nickname || message.author.username;
 
-    messages.push({ sender: nickname, message: message.content });
-    if (messages.length > 50) messages.shift();
+      messages.push({ sender: nickname, message: message.content });
+      if (messages.length > 50) messages.shift();
+    } catch (err) {
+      console.error('Fehler beim Laden des Nicknames:', err);
+    }
   }
 });
 
