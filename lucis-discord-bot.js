51b563e985e2508907465b551dab3559df75562a
Vita-Lucis <Vita@lucis.family>
2025-05-03 13:50:08 +0200

    Update lucis-discord-bot.js

--- lucis-discord-bot.js
+++ lucis-discord-bot.js
@@ -133,6 +133,8 @@ client.on('messageCreate', async (message) => {
     id
   };
 
+  // Wenn die Nachricht von Discord zum Webchat kommt, senden wir sie nicht zurück in den Discord-Channel.
+  if (message.content.includes("http://lucis.family")) {
   await fetch('https://br-cke.onrender.com/send', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
